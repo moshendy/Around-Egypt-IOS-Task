4d2d5ade0b72d7834c010b5c56559eb50cfb2cc7
Mohamed Shendy <mohamedshendy@Shendys-MacBook-Air.local>
2025-07-18 03:25:53 +0300

    first commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Mohamed Shendy on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AroundEgypt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AroundEgypt
+//
+//  Created by Mohamed Shendy on 18/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

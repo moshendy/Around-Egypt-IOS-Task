d2fb3cdf948a9e0f6ea1b781d5adafc45c78c559
Mohamed Shendy <mohamedshendy@Shendys-MacBook-Air.local>
2025-07-18 04:21:24 +0300

    added experience model

---
+++ AroundEgypt/Models/Experience.swift
@@ -0,0 +1,65 @@
+//
+//  Experience.swift
+//  AroundEgypt
+//
+//  Created by Mohamed Shendy on 18/07/2025.
+//
+        
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let meta: Meta
+    let data: [Experience]
+    let pagination: Pagination
+}
+
+// MARK: - Datum
+struct Experience: Codable {
+    let id, title: String
+    let coverPhoto: String
+    let description: String
+    let viewsNo, likesNo, recommended, hasVideo: Int
+    let city: City
+    let tourHTML: String
+    let detailedDescription, address: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case coverPhoto = "cover_photo"
+        case description
+        case viewsNo = "views_no"
+        case likesNo = "likes_no"
+        case recommended
+        case hasVideo = "has_video"
+        case city
+        case tourHTML = "tour_html"
+        case detailedDescription = "detailed_description"
+        case address
+
+    }
+}
+
+// MARK: - City
+struct City: Codable {
+    let id: Int
+    let name: String
+    let topPick: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case topPick = "top_pick"
+    }
+}
+
+
+// MARK: - Meta
+struct Meta: Codable {
+    let code: Int
+//    let errors: [JSONAny]
+}
+
+// MARK: - Pagination
+struct Pagination: Codable {
+}
+
